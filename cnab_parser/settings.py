628267a3633a1a621bbb62d0abbabec34b311e5f
VictrCruz312 <motoggb979@gmail.com>
2023-01-23 15:06:54 -0300

    feat: Adicionando o app cnab_upload e o modulo do rest_framework nos installed_apps do settings.py

--- cnab_parser/settings.py
+++ cnab_parser/settings.py
@@ -30,7 +30,11 @@ ALLOWED_HOSTS = []
 
 # Application definition
 
-INSTALLED_APPS = [
+MY_APPS = ["cnab_upload"]
+
+THIRD_PARTY_APPS = ["rest_framework"]
+
+DJANGO_APPS = [
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
@@ -38,6 +42,8 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
 ]
+
+INSTALLED_APPS = MY_APPS + DJANGO_APPS + THIRD_PARTY_APPS
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
