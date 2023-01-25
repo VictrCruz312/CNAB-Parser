c5d5fabeebd40e3084f2237a14aa98cc46909a0b
VictrCruz312 <motoggb979@gmail.com>
2023-01-25 14:30:59 -0300

    feat: removendo a rota listMovimentation para renderizar a lista junto do formulário

--- cnab_upload/urls.py
+++ cnab_upload/urls.py
@@ -1,7 +1,4 @@
 from django.urls import path
 from .views import UploadFileView, ItemListView
 
-urlpatterns = [
-    path("upload/", UploadFileView.as_view(), name="upload"),
-    path("upload/listMovimentation/", ItemListView.as_view(), name="list"),
-]
+urlpatterns = [path("upload/", UploadFileView.as_view(), name="upload")]
